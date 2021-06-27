0c696b6f38ca72d4b3121d5c14944db420e362d1
Cori Lint <corilint@gmail.com>
2021-06-27 16:08:45 -0500

    installed Tailwind and built Navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    return <h1>NavBar pageeeee</h1>
+    return (
+        <header className="bg-purple-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" exact className="inflex-flex items-center py-6 px-3 mr-4 text-purple-100 hover:text-white text-4xl font-bold mono tracking-widest">
+                        Home
+                    </NavLink>
+                    <NavLink to="/post">
+                        Blog
+                    </NavLink>
+                    <NavLink to="/project">
+                        Projects
+                    </NavLink>
+                    <NavLink to="/about">
+                        About
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
 }

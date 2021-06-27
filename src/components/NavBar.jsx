85d576ba9b9faa8ad6008ab62b3fd973b94fa209
Cori Lint <corilint@gmail.com>
2021-06-27 16:14:57 -0500

    styled nav links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,16 +6,32 @@ export default function NavBar() {
         <header className="bg-purple-600">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
-                    <NavLink to="/" exact className="inflex-flex items-center py-6 px-3 mr-4 text-purple-100 hover:text-white text-4xl font-bold mono tracking-widest">
+                    <NavLink 
+                        to="/" exact 
+                        activeClassName="text-white"
+                        className="inline-flex items-center py-6 px-3 mr-4 text-purple-100 hover:text-green-800 text-4xl font-bold mono tracking-widest"
+                    >
                         Home
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink 
+                        to="/post"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-200 hover:text-green-800"
+                        activeClassName="text-purple-100 bg-purple-700"
+                    >
                         Blog
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink 
+                        to="/project"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-200 hover:text-green-800"
+                        activeClassName="text-purple-100 bg-purple-700"
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink 
+                        to="/about"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-200 hover:text-green-800"
+                        activeClassName="text-purple-100 bg-purple-700"
+                    >
                         About
                     </NavLink>
                 </nav>

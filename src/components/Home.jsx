d1583eee1a3e2ed28def00363279a314ed7ffdfc
Cori Lint <corilint@gmail.com>
2021-07-13 14:55:00 -0500

    added bg image, section, and title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react"
+import image from "../img/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.jpg"
 
 export default function Home() {
-    return <h1>Home pageeeee</h1>
+    return (
+        <main>
+            <img src={image} alt="purple clouds" 
+                className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold mono leading-none lg:leading-snug home-name">Hi, I'm Cori</h1>
+            </section>
+        </main>
+    )
 }

627c18b02c5ba4cc58a5d6641ca641c3ebf9033f
Cori Lint <corilint@gmail.com>
2021-07-21 14:34:18 -0500

    set up Sanity data fetch with GROQ query

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+
+// pull in data from Sanity studio
+// use GROQ - Sanity in-house query language similar to GraphQL
 
 export default function Post() {
-    return <h1>Post pageeeee</h1>
+    const [postData, setPost] = useState(null);
+
+    useEffect(() => {
+        sanityClient //GROQ query is within backticks
+            .fetch(`*[_type == "post"]{ 
+                title,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    },
+                    alt
+                }
+            }`) 
+            .then((data) => setPost(data)) 
+    }, []);
+
+    return 
 }

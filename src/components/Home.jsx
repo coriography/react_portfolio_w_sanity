13a0705270ed682c1caee0032121c483f95ee168
Cori Lint <corilint@gmail.com>
2021-07-21 14:33:31 -0500

    styled main text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <img src={image} alt="purple clouds" 
                 className="absolute object-cover w-full h-full" />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold mono leading-none lg:leading-snug home-name">Hi, I'm Cori</h1>
+                <h1 className="text-6xl text-purple-100 font-bold mono leading-none lg:leading-snug home-name">Hi, I'm Cori</h1>
             </section>
         </main>
     )

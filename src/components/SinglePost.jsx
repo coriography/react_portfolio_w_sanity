d489023bb0d9b33f8f839b1316bc5fb16132979d
Cori Lint <corilint@gmail.com>
2021-07-25 20:08:21 -0500

    set up Single Post fetch, image URL builder, and content loop

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source);
+}
 
 export default function SinglePost() {
-    return <h1>SinglePost pageeeee</h1>
+    const [singlePost, setSinglePost] = useState(null);
+    const { slug } = useParams();
+
+    useEffect(() => {
+        sanityClient.fetch(`*[slug.current == "${slug}"]{
+            title,
+            _id,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                }
+            },
+            body,
+            "authorName": author->name,
+            "authorImage": author->image
+        }`).then((data) => setSinglePost(data[0])) // ^^ aliases
+        .catch(console.error);
+    }, [slug]);
+
+    if (!singlePost) return <div>Loading...</div>;
+
+    return ;
 }

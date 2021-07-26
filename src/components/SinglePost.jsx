5e813ea68344540ff55cf66febb0c78bf82ed95e
Cori Lint <corilint@gmail.com>
2021-07-25 20:21:05 -0500

    added HTML and helper classes for single post page

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -27,10 +27,36 @@ export default function SinglePost() {
             "authorName": author->name,
             "authorImage": author->image
         }`).then((data) => setSinglePost(data[0])) // ^^ aliases
+        // TODO: check sanity docs for image URL builder styling - chain on width, etc.
         .catch(console.error);
     }, [slug]);
 
     if (!singlePost) return <div>Loading...</div>;
 
-    return ;
+    return (
+        <main className="bg-gray-200 min-h-screen p-12">
+            <article className="container shadow-lg mx-auto bg-purple-100 rounded-lg">
+                <header className="relative">
+                    <div className="absolute h-full w-full flex items-center justify-center p-8">
+                        <div className="bg-white bg-opacity-75 rounded p-12">
+                            <h1 className="mono text-3xl lg:text-6xl mb-4">
+                                {singlePost.title}
+                            </h1>
+                            <div className="flex justify-center text-gray-800">
+                                <img src={urlFor(singlePost.authorImage).url()} alt={singlePost.name}  className="w-10 h-10 rounded-full"/>
+                                <p className="mono flex items-center pl-2 text-2xl">{singlePost.authorName}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img 
+                    src={singlePost.mainImage.asset.url} 
+                    alt={singlePost.title} 
+                    className="w-full object-cover rounded-t"
+                    style={{ height: "400px" }}
+                    />
+                </header>
+                <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg: prose-xl max-w-full">BLOCK CONTENT</div>
+            </article>
+        </main>
+    );
 }

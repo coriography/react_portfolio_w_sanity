a8dc745d700a5625f6f2836ca531b9d550ea8da5
Cori Lint <corilint@gmail.com>
2021-08-08 15:36:19 -0500

    added author fetch query

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MappingsContext } from "source-list-map";
+import sanityClient from "../client.js";
+import image from "../img/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.jpg";
 
 export default function About() {
-    return <h1>About pageeeee</h1>
+    const [author, setAuthor] = useState(null);
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, [])
+
+    if (!author) return <div>Loading...</div>
+
+    return (
+        null
+    )
 }

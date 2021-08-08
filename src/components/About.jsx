2f1fbef926ed33512fb4263e0e51fa7c62e1fbe6
Cori Lint <corilint@gmail.com>
2021-08-08 15:44:06 -0500

    completed About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from "react";
 import { MappingsContext } from "source-list-map";
 import sanityClient from "../client.js";
 import image from "../img/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source);
+}
 
 export default function About() {
     const [author, setAuthor] = useState(null);
@@ -21,11 +28,14 @@ export default function About() {
             <img src={image} alt="purple clouds" className="absolute w-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-purple-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} alt="" />
-                    <div>
-                        <h1></h1>
-                        <div>
-                            BLOCK CONTENT
+                    <img src={urlFor(author.authorImage).url()} alt={author.name} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/>
+                    <div className="text-lg flex flex-col justify-center">
+                        <h1 className="mono text-6xl text-purple-300 mb-4">
+                            Hi there. I'm{" "}
+                            <span className="text-purple-100">{author.name}</span>
+                        </h1>
+                        <div className="prose lg:prose-xl text-white">
+                            <BlockContent blocks={author.bio} projectId="5e46qv2t" dataset="production" />
                         </div>
                     </div>
                 </section>
